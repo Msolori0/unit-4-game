14e128936a4d44576353c4a1472e0d9991277c52
Solorio <solorio_michelle@yahoo.com>
2018-04-08 21:03:58 -0400

    final update

---
+++ assets/javascript/game.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+
+    //global variables
+    
+var wins=0;
+var losses=0;
+var gemNumbers=0;
+
+// creating the random number by the computer 
+var computerNumber= (Math.floor(Math.random() * 120)+ 9);
+console.log("computer", computerNumber);
+
+
+//making the computers number appear on the page
+$("#numberbox").text(computerNumber);
+
+//setting certain variables to randomly change
+var gemOne= (Math.floor(Math.random() * 12)+ 1);
+console.log(gemOne);
+var gemTwo= (Math.floor(Math.random() * 12)+ 1);
+console.log(gemTwo);
+var gemThree= (Math.floor(Math.random() * 12)+ 1);
+console.log(gemThree);
+var gemFour= (Math.floor(Math.random() * 12)+ 1);
+console.log(gemFour);
+
+$("#gem1").on('click', function(){
+
+    gemNumbers= gemNumbers + gemOne;
+    $("#totalscore").text(gemNumbers);
+
+    if (gemNumbers === computerNumber) {
+        winner();
+    }
+    else if(gemNumbers > computerNumber) {
+        loser();}
+    });
+
+$("#gem2").on('click', function(){
+
+    gemNumbers= gemNumbers + gemTwo;
+    $("#totalscore").text(gemNumbers);
+    
+    if (gemNumbers === computerNumber) {
+        winner();
+    }
+    else if(gemNumbers > computerNumber) {
+        loser();}
+    });
+
+$("#gem3").on('click', function(){
+
+        gemNumbers= gemNumbers + gemThree;
+        $("#totalscore").text(gemNumbers);
+        
+        if (gemNumbers === computerNumber) {
+            winner();
+        }
+        else if(gemNumbers > computerNumber) {
+            loser();}
+        });
+
+$("#gem4").on('click', function(){
+
+        gemNumbers = gemNumbers + gemFour;
+            $("#totalscore").text(gemNumbers);
+            
+        if (gemNumbers === computerNumber) {
+                winner();
+            }
+        else if(gemNumbers > computerNumber) {
+                loser();
+            }
+            });
+
+function winner(){
+    wins++;
+    $('#wins').text("Wins: "+ wins);
+    reset();
+}; 
+
+function loser(){
+    losses++;
+    $('#losses').text("Losses: "+losses);
+    reset();
+}
+
+function reset(){
+    var computerNumber= (Math.floor(Math.random() * 120)+ 9);
+    var gemOne= (Math.floor(Math.random() * 12)+ 1);
+    var gemTwo= (Math.floor(Math.random() * 12)+ 1);
+    var gemThree= (Math.floor(Math.random() * 12)+ 1);
+    var gemFour= (Math.floor(Math.random() * 12)+ 1);
+    $("#totalscore").text(gemNumbers);
+    $("#numberbox").text(computerNumber);
+    gemNumbers=0;
+
+};
+
+});
+
+
